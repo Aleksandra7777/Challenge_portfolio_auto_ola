13fa7c34625a9abac84a4a148bc944f4250b513e
Aleksandra7777 <fujak.aleksandra@gmail.com>
2023-07-04 23:35:33 +0200

    commit

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,5 +2,14 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-    button_xpath = "//*[@id='login']"
+    logo_xpath = "//*[contains(@class, 'MuiCardMedia-root jss8')]"
+    activity_xpath = "//*[text()='Activity']"
+    shortcuts_xpath = "//*[text()='Shortcuts']"
+    add_player_xpath = "//*[text()='Add player']"
+    sign_out_xpath = "//*[text()='Sign out']"
+    players_xpath = "//*[text()='Players']"
+    main_page_xpath = "//*[text()='Main page']"
+    players_count_xpath = "//*[text()='Players count']"
+    dev_team_contact_xpath = "//*[text()='Dev team contact']"
+    scotus_panel_xpath = "//*[contains(@class, 'MuiTypography-root jss144')]"
     pass
